f75263c1df11702e9486aad97a233bea0325e94d
Ross Evans <therealrossevans@gmail.com>
2018-07-05 20:08:58 -0400

    Remove pwd, use https to clone instead of ssh

--- deploy-website.sh
+++ deploy-website.sh
@@ -1,10 +1,9 @@
 cd /usr/share/nginx
-git clone git@github.com:Qcode/Portfolio.git
+git clone https://github.com/Qcode/Portfolio.git
 cd Portfolio
 yarn
 yarn run build
 cd ..
-pwd
 rm -rf html/static
 mv -v Portfolio/build/* html
 rm -rf Portfolio
